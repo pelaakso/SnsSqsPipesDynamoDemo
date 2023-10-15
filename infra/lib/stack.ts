dbea690cdaff82127b1a84cb8eaca67741ae421b
pelaakso <pelaakso@users.noreply.github.com>
2023-10-15 13:53:02 +0000

    Replace deprecated SF property

--- infra/lib/stack.ts
+++ infra/lib/stack.ts
@@ -3,7 +3,14 @@ import { Construct } from 'constructs';
 import { Topic } from 'aws-cdk-lib/aws-sns';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { SqsSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
-import { JsonPath, LogLevel, Pass, StateMachine, StateMachineType } from 'aws-cdk-lib/aws-stepfunctions';
+import {
+  DefinitionBody,
+  JsonPath,
+  LogLevel,
+  Pass,
+  StateMachine,
+  StateMachineType,
+} from 'aws-cdk-lib/aws-stepfunctions';
 import { CfnPipe } from 'aws-cdk-lib/aws-pipes';
 import { PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
@@ -88,7 +95,7 @@ export class PipesTestStack extends Stack {
     );
 
     const stateMachine = new StateMachine(this, 'PipesTestStateMachine', {
-      definition: passState,
+      definitionBody: DefinitionBody.fromChainable(passState),
       stateMachineName: 'pipes-test-state-machine',
       stateMachineType: StateMachineType.EXPRESS,
       timeout: Duration.minutes(1),
